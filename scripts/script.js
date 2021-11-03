f708ef4324ec71623d3a000baeabc42b00e6fff3
Charles Yiu <charlesyiu.w@gmail.com>
2021-11-03 18:22:50 +0800

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -2,19 +2,22 @@
 let elementIds = null
 let presetLineColors = null
 const configUrl = window.location.hostname != "charlesyiu.github.io" ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}/config.json` : "https://cdn.jsdelivr.net/gh/charlesyiu/draw@latest/config.json"
-fetch(configUrl)
-    .then(response => response.json())
-    .then(json => {
-        elementIds = json.elementIds
-        presetLineColors = json.presetLineColors
-        document.getElementById("loading-notice").hidden = true
-        document.getElementById("shapes").hidden = false
-        document.getElementById("settings").hidden = false
-        setTool(scribbleTool)
-        initializeTools()
-        initializeSettings()
-    })
-if (!elementIds) {
-    document.getElementById("shapes").hidden = true
-    document.getElementById("settings").hidden = true
+function initialize() {
+    fetch(configUrl)
+        .then(response => response.json())
+        .then(json => {
+            elementIds = json.elementIds
+            presetLineColors = json.presetLineColors
+            document.getElementById("loading-notice").hidden = true
+            document.getElementById("shapes").hidden = false
+            document.getElementById("settings").hidden = false
+            setTool(scribbleTool)
+            initializeTools()
+            initializeSettings()
+        })
+    if (!elementIds) {
+        document.getElementById("shapes").hidden = true
+        document.getElementById("settings").hidden = true
+    }
 }
+window.onload = initialize
