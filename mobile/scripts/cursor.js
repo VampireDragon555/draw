7a6c938ba48995560b865d278fdeb1e6b65f8315
Charles Yiu <charlesyiu.w@gmail.com>
2021-11-04 16:03:05 +0800

    cursor changes

--- mobile/scripts/cursor.js
+++ mobile/scripts/cursor.js
@@ -1,16 +1,27 @@
 function updateCursor() {
-    const cursorPosition = window.event.touches
+    const cursorPosition = [window.event]
     if (cursorPosition[0] === undefined) { return }
+
     const cursorElement = document.getElementById(elementIds.cursor)
     const cursorDotElement = document.getElementById(elementIds.cursorDot)
+
+    cursorElement.hidden = false
+
     cursorElement.style.width = `${thickness + 2}px`
     cursorElement.style.height = `${thickness + 2}px`
 
-    cursorElement.style.left = `${cursorPosition[0].clientX - (thickness / 2) - 2}px`
-    cursorElement.style.top = `${cursorPosition[0].clientY - (thickness / 2) - 2}px`
+    const leftStyle = `${cursorPosition[0].clientX - (thickness / 2) - 2}px`
+    const topStyle = `${cursorPosition[0].clientY - (thickness / 2) - 2}px`
+    cursorElement.style.left = leftStyle
+    cursorElement.style.top = topStyle
     cursorDotElement.style.left = `${(thickness - 2) / 2}px`
     cursorDotElement.style.top = `${(thickness - 2) / 2}px`
+
+    setTimeout(() => {
+        if (cursorElement.style.left === leftStyle && cursorElement.style.top === topStyle) { cursorElement.hidden = true }
+    }, 2500)
 }
 function initializeCursor() {
-    document.body.ontouchmove = updateCursor
+    document.body.onmousemove = updateCursor
+    document.getElementById(elementIds.cursor).hidden = true
 }
